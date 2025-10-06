d5f57439f87a50e25d03b0bcdf4d95c4a549e3d3
syh053 <syh053@gmail.com>
2025-10-06 23:45:59 +0800

    add migration: add isComplete column into todos table

--- db/models/todos.py
+++ db/models/todos.py
@@ -1,8 +1,10 @@
+from xmlrpc.client import boolean
 from sqlmodel import SQLModel, Field
 from datetime import datetime
 
 class Todos(SQLModel, table=True):
   id: int |None = Field(default=None, primary_key=True)
   name: str = Field(nullable=False)
+  isComplete: boolean = Field(default=False, nullable=False)
   created_at: datetime = Field(default_factory=datetime.now)
   updated_at: datetime = Field(default_factory=datetime.now)
