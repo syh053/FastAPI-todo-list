9fcab8c45cb5c2d5d8e13c5b4ecd8b6418aeadad
syh053 <syh053@gmail.com>
2025-10-20 21:53:23 +0800

    modify db seed file

--- db/seeds/todos.py
+++ db/seeds/todos.py
@@ -1,17 +1,26 @@
+import asyncio # 非同步程式執行框架
 from pathlib import Path
 import sys
 
 package_path = str(Path().cwd().resolve() / "db")
 sys.path.append(package_path)
 
-from models import get_session
+from models import get_session, asyne_engine # 加入非同步 engine
 from models.todos import Todos
 
 seed_data = [Todos(name=f"Todo {i}") for i in range(1, 11)]
 
-session = next(get_session())
+async def main():
+    async for session in get_session():
+        session.add_all(seed_data)
+        await session.commit()
+        await session.close()
 
-session.add_all(seed_data)
-session.commit()
+    # 關閉引擎
+    await asyne_engine.dispose()
 
-print("Seed 資料已經新增完成")
+    print("Seed 資料已經新增完成")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
